84b1386494775fa2e8068927b12b482c8c386fcf
AsqrB <128715896+AsqrB@users.noreply.github.com>
2023-04-14 18:18:08 +0530

    Create Answer.py

---
+++ Q#4/Answer.py
@@ -0,0 +1,21 @@
+#Boolean function to check for digit repetition in a number
+def fun(s):
+    s = ''.join(sorted(s))  #Sorting elements of string
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            return False     #If the digits repeat
+        
+    return True  #If digits doesnot repeat
+
+#Taking inputs from user
+lB = int(input())  #For lower bound
+uB = int(input())  #For upper bound
+
+count = 0  #To count numbers without digit repetitioin 
+
+for i in range(lB,uB+1):
+    if(fun(str(i))):  #if digits doesnot repeat
+        count + = 1   #Incrementing value of count
+        
+#Printing number of integers in given range without digit Repetition
+print(count)
